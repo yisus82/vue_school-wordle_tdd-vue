726da7bcd8b0d9a79a9c156bafe76821cdee7b29
yisus82 <djalma8@gmail.com>
2024-03-24 21:18:37 +0100

    Display defeat message

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -12,4 +12,16 @@ describe('WordleBoard', () => {
 
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
+
+  test('a defeat message appears when the user makes a guess that does not match the word of the day', async () => {
+    const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } })
+
+    const guessInput = wrapper.find('input[type="text"]')
+    await guessInput.setValue('WRONG')
+    await guessInput.trigger('keydown.enter')
+
+    expect(wrapper.text()).toContain('Better luck next time!')
+  })
+
+  test.todo('no message appears when the user has not yet made a guess')
 })

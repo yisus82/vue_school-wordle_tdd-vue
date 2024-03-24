f833c14159df5ed845cded4b87341cc73493a72d
yisus82 <djalma8@gmail.com>
2024-03-24 20:57:53 +0100

    Use “Arrange, Act, Assert” structure in test

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -4,9 +4,11 @@ import WordleBoard from '../WordleBoard.vue'
 describe('WordleBoard', () => {
   test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
     const wrapper = mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } })
+
     const guessInput = wrapper.find('input[type="text"]')
     await guessInput.setValue('TESTS')
     await guessInput.trigger('keydown.enter')
+
     expect(wrapper.text()).toContain('You won!')
   })
 })

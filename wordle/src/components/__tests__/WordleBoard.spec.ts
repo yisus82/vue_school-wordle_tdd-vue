4577934b43dcf632a7188576d78f2666dd195493
yisus82 <djalma8@gmail.com>
2024-03-24 22:23:00 +0100

    Organize tests into describe blocks

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -16,52 +16,66 @@ describe('WordleBoard', () => {
     wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
   })
 
-  test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    await playerSubmitsGuess(wordOfTheDay)
+  describe('End of game messages', () => {
+    test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
+      await playerSubmitsGuess(wordOfTheDay)
 
-    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+      expect(wrapper.text()).toContain(VICTORY_MESSAGE)
+    })
+
+    test('a defeat message appears when the user makes a guess that does not match the word of the day', async () => {
+      await playerSubmitsGuess('WRONG')
+
+      expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+    })
+
+    test('no message appears when the user has not yet made a guess', async () => {
+      expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
+      expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
+    })
   })
 
-  test('a defeat message appears when the user makes a guess that does not match the word of the day', async () => {
-    await playerSubmitsGuess('WRONG')
+  describe('Rules for defining the word of the day', () => {
+    test('if the word of the day provided does not have exactle 5 characters, a warning is emitted', () => {
+      console.warn = vi.fn()
 
-    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+      mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } })
+
+      expect(console.warn).toHaveBeenCalled()
+    })
+
+    test('if the word of the day is not in uppercase, a warning is emitted', () => {
+      console.warn = vi.fn()
+
+      mount(WordleBoard, { props: { wordOfTheDay: 'Lower' } })
+
+      expect(console.warn).toHaveBeenCalled()
+    })
+
+    test('if the word of the day is not a real word in English, a warning is emitted', () => {
+      console.warn = vi.fn()
+
+      mount(WordleBoard, { props: { wordOfTheDay: 'QWERT' } })
+
+      expect(console.warn).toHaveBeenCalled()
+    })
+
+    test('no warning is emitted if the word of the day is a real uppercase word in English with 5 characters', () => {
+      console.warn = vi.fn()
+
+      mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } })
+
+      expect(console.warn).not.toHaveBeenCalled()
+    })
   })
 
-  test('no message appears when the user has not yet made a guess', async () => {
-    expect(wrapper.text()).not.toContain(VICTORY_MESSAGE)
-    expect(wrapper.text()).not.toContain(DEFEAT_MESSAGE)
-  })
+  describe('Player guesses', () => {
+    test.todo('player guesses are limited to 5 characters')
 
-  test('if the word of the day provided does not have exactle 5 characters, a warning is emitted', () => {
-    console.warn = vi.fn()
+    test.todo('player guesses can only be submitted if they are real words in English')
 
-    mount(WordleBoard, { props: { wordOfTheDay: 'TEST' } })
+    test.todo('player guesses are not case-sensitive')
 
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test('if the word of the day is not in uppercase, a warning is emitted', () => {
-    console.warn = vi.fn()
-
-    mount(WordleBoard, { props: { wordOfTheDay: 'Lower' } })
-
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test('if the word of the day is not a real word in English, a warning is emitted', () => {
-    console.warn = vi.fn()
-
-    mount(WordleBoard, { props: { wordOfTheDay: 'QWERT' } })
-
-    expect(console.warn).toHaveBeenCalled()
-  })
-
-  test('no warning is emitted if the word of the day is a real uppercase word in English with 5 characters', () => {
-    console.warn = vi.fn()
-
-    mount(WordleBoard, { props: { wordOfTheDay: 'TESTS' } })
-
-    expect(console.warn).not.toHaveBeenCalled()
+    test.todo('player guesses can only contain letters')
   })
 })

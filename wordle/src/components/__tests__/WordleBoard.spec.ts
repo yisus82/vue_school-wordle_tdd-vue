f0f4160e9e5daffcffde49c9877022c46151993d
yisus82 <djalma8@gmail.com>
2024-03-24 21:39:39 +0100

    Extract method

--- wordle/src/components/__tests__/WordleBoard.spec.ts
+++ wordle/src/components/__tests__/WordleBoard.spec.ts
@@ -6,22 +6,24 @@ describe('WordleBoard', () => {
   const wordOfTheDay = 'TESTS'
   let wrapper: ReturnType<typeof mount>
 
+  const playerSubmitsGuess = async (guessWord: string) => {
+    const guessInput = wrapper.find('input[type="text"]')
+    await guessInput.setValue(guessWord)
+    await guessInput.trigger('keydown.enter')
+  }
+
   beforeEach(() => {
     wrapper = mount(WordleBoard, { props: { wordOfTheDay } })
   })
 
   test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    const guessInput = wrapper.find('input[type="text"]')
-    await guessInput.setValue('TESTS')
-    await guessInput.trigger('keydown.enter')
+    await playerSubmitsGuess(wordOfTheDay)
 
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 
   test('a defeat message appears when the user makes a guess that does not match the word of the day', async () => {
-    const guessInput = wrapper.find('input[type="text"]')
-    await guessInput.setValue('WRONG')
-    await guessInput.trigger('keydown.enter')
+    await playerSubmitsGuess('WRONG')
 
     expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
   })

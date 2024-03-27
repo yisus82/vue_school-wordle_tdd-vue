59ac6b8a2f9269e9cd42c896f42bf232d7492c93
yisus82 <djalma8@gmail.com>
2024-03-27 12:18:26 +0100

    Change end of game messages

--- wordle/src/settings.ts
+++ wordle/src/settings.ts
@@ -1,4 +1,4 @@
-export const VICTORY_MESSAGE = 'You won!'
-export const DEFEAT_MESSAGE = 'Better luck next time!'
+export const VICTORY_MESSAGE = '🎉 Congratulations!!! 🎉'
+export const DEFEAT_MESSAGE = 'Better luck next time'
 export const WORD_LENGTH = 5
 export const LANGUAGE = 'english'

cff7c3ce75cd533c0d75b24041bcc95ddf9764c8
yisus82 <djalma8@gmail.com>
2024-03-29 12:20:05 +0100

    Extract GuessView component

--- wordle/src/components/GuessInput.vue
+++ wordle/src/components/GuessInput.vue
@@ -3,6 +3,7 @@ const { default: validWords } = await import(`../${LANGUAGE}WordsWith${WORD_LENG
 </script>
 
 <script setup lang="ts">
+import GuessView from '@/components/GuessView.vue';
 import { LANGUAGE, WORD_LENGTH } from '@/settings';
 import { ref } from 'vue';
 
@@ -45,10 +46,7 @@ const handleSubmit = () => {
 </script>
 
 <template>
-  <ul class="word">
-    <li v-for="(letter, index) in guessInProgress.padEnd(WORD_LENGTH, ' ')" :key="`${letter}-${index}`"
-      :data-letter="letter" class="letter" v-text="letter"></li>
-  </ul>
+  <guess-view :guess="guessInProgress" />
   <input id="guessInput" type="text" :value="guessInProgress" @keydown.enter="handleSubmit" @input="formatInput"
     :maxlength="WORD_LENGTH" autofocus @blur="({ target }) => (target as HTMLInputElement).focus()" />
 </template>
@@ -57,38 +55,5 @@ const handleSubmit = () => {
 input {
   position: absolute;
   opacity: 0;
-}
-
-.word {
-  display: flex;
-  list-style-type: none;
-  padding: 0;
-  gap: 0.25rem;
-}
-
-.letter {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 5rem;
-  height: 5rem;
-  font-size: 2rem;
-  font-weight: bolder;
-  border: 1px solid hsl(0, 0%, 70%);
-  background-color: white;
-}
-
-@keyframes pop {
-  0% {
-    transform: scale(1);
-  }
-
-  50% {
-    transform: scale(1.4);
-  }
-}
-
-li:not([data-letter=" "]) {
-  animation: pop 100ms;
 }
 </style>
---
+++ wordle/src/components/GuessView.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { WORD_LENGTH } from '@/settings';
+
+defineProps<{ guess: string; }>();
+</script>
+
+<template>
+  <ul class="word">
+    <li v-for="(letter, index) in guess.padEnd(WORD_LENGTH, ' ')" :key="`${letter}-${index}`" :data-letter="letter"
+      class="letter" v-text="letter"></li>
+  </ul>
+</template>
+
+<style scoped>
+.word {
+  display: flex;
+  list-style-type: none;
+  padding: 0;
+  gap: 0.25rem;
+}
+
+.letter {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 5rem;
+  height: 5rem;
+  font-size: 2rem;
+  font-weight: bolder;
+  border: 1px solid hsl(0, 0%, 70%);
+  background-color: white;
+}
+
+@keyframes pop {
+  0% {
+    transform: scale(1);
+  }
+
+  50% {
+    transform: scale(1.4);
+  }
+}
+
+li:not([data-letter=" "]) {
+  animation: pop 100ms;
+}
+</style>
